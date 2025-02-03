8d1c6b8bde286fcdf844773a150cac2bb9f2b33b
veresur <veres.tamas@pm.me>
2025-02-03 20:51:44 +0100

    FINK-8 auth working

---
+++ src/services/FinTrack-api/fintrack-api/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using fintrack_api_business_logic.Handlers;
+using fintrack_common.DTO.AuthDTO;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fintrack_api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IMediator _mediator;
+        public AuthController (IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login(CancellationToken cancellationToken)
+        {
+            string idToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            return Ok("gggggg");
+
+            
+        }
+    }
+}
--- src/services/FinTrack-api/fintrack-api/Program.cs
+++ src/services/FinTrack-api/fintrack-api/Program.cs
@@ -1,7 +1,55 @@
 using Microsoft.EntityFrameworkCore;
 using fintrack_database.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using fintrack_api_business_logic;
+using fintrack_common.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using fintrack_common.Providers;
+using System.Security.Claims;
+using fintrack_api;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyHeader();
+            builder.AllowAnyOrigin();
+            builder.AllowAnyMethod();
+            /*builder.WithOrigins(
+                "*",
+                "https://*.graphisoft.hu",
+                "http://localhost:4200",
+                "https://localhost:4200",
+                "https://127.0.0.1:4200",
+                "https://127.0.0.1:4200",
+                "http://buildforge-local:4200",
+                "https://buildforge-local:4200"
+                )
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+                    //.WithExposedHeaders(HeaderNames.Location, "Location")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();*/
+        }
+    );
+});
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(BusinessLogicAssemblyMarker).Assembly);
+});
+
+builder.Services.Configure<AuthenticationConfiguration>(config =>
+{
+    config.Secret = builder.Configuration.GetSection("JWT_SECRET").Value!;
+    config.Lifetime = 10;
+    config.Auth0Authority = builder.Configuration.GetSection("AUTH0_Authority").Value!;
+    config.Auth0Audience = builder.Configuration.GetSection("AUTH0_Audience").Value!;
+});
 
 // Add services to the container.
 
@@ -17,7 +65,27 @@ builder.Services.AddDbContext<FinTrackDatabaseContext>(options =>
     options.UseMySql(databaseConnString, ServerVersion.AutoDetect(databaseConnString));
 });
 
+builder.Services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration.GetSection("AUTH0_Authority").Value!;
+    options.Audience = builder.Configuration.GetSection("AUTH0_Audience").Value!;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+});
+
 var app = builder.Build();
+
+app.UseMiddleware<RequestLoggingMiddleware>();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,7 +96,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthentication();
 
 app.MapControllers();
 
